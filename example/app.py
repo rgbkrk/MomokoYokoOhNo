18de7ec6c543cbe65346ac0086058c5875b302c2
Kyle Kelley <kyle.kelley@rackspace.com>
2014-03-13 13:08:54 -0500

    Add options, fetch simple query

--- example/app.py
+++ example/app.py
@@ -4,6 +4,10 @@
 from tornado import gen
 from tornado.ioloop import IOLoop
 from tornado.httpserver import HTTPServer
+
+import tornado.options
+from tornado.options import define, options
+
 from tornado.options import parse_command_line
 from tornado.web import *
 
@@ -18,24 +22,39 @@ class BaseHandler(RequestHandler):
 
 
 class TutorialHandler(BaseHandler):
+    @asynchronous
     def get(self):
-        self.write('Some text here!')
+        self.db.execute('SELECT 1;', callback=self._done)
+
+    def _done(self, cursor, error):
+        self.write('Results: %r' % (cursor.fetchall(),))
         self.finish()
 
 
 if __name__ == '__main__':
+    define("dbname", default="postgres", help="Database name")
+    define("dbuser", default="postgres", help="Postgres user")
+    define("dbpass")
+    define("dbhost", default="localhost")
+    define("dbport", default="5432")
+
     parse_command_line()
+
     application = Application([
         (r'/', TutorialHandler)
     ], debug=True)
 
+    dsn_template = ('dbname={} user={} password={}'
+                    'host={} port={}')
+
+    dsn = dsn_template.format(options.dbname, options.dbuser, options.dbpass,
+                              options.dbhost, options.dbport)
+
     application.db = momoko.Pool(
-        dsn='dbname=your_db user=your_user password=very_secret_password '
-            'host=localhost port=5432',
+        dsn=dsn,
         size=1
     )
 
     http_server = HTTPServer(application)
     http_server.listen(8888, 'localhost')
     IOLoop.instance().start()
-
